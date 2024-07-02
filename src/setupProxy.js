e9a2c5de69cab96dd0670df2a8e03200a62e8e02
choizeus02 <choizeus0212@gmail.com>
2024-07-03 01:49:03 +0900

    jenkins test url local to dev2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware( {
-      target: "http://localhost:9000",
+      target: "http://116.121.184.161",
       changeOrigin: true,
     })
   );
